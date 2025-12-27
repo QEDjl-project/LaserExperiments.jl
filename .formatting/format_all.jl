97edc60baf00893de3fa2e5742c21f3e31b54959
Uwe Hernandez Acosta <szabo137@users.noreply.github.com>
2025-12-27 13:27:23 +0100

    Cleanup and beautification (#25)
    * added readme as landing page for docs
    
    * cleanup tests
    
    * add docs meta
    
    ---------
    
    Co-authored-by: Uwe Hernandez Acosta <u.hernandez@hzdr.de>

--- .formatting/format_all.jl
+++
@@ -1,9 +0,0 @@
-using JuliaFormatter
-
-not_formatted = format(pwd(); verbose = true)
-if not_formatted
-    @info "Formatting verified."
-else
-    @warn "Formatting verification failed: Some files are not properly formatted!"
-end
-exit(not_formatted ? 0 : 1)
--- docs/make.jl
+++ docs/make.jl
@@ -1,36 +1,63 @@
-using LaserExperiments
+using Pkg
+
+# targeting the correct source code
+# this assumes the make.jl script is located in LaserExperiments.jl/docs
+project_path = Base.Filesystem.joinpath(Base.Filesystem.dirname(Base.source_path()), "..")
+Pkg.develop(; path = project_path)
+
 using Documenter
+using LaserExperiments
 
 DocMeta.setdocmeta!(LaserExperiments, :DocTestSetup, :(using LaserExperiments); recursive = true)
 
-const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
-const numbered_pages = [
-    file for file in readdir(joinpath(@__DIR__, "src")) if
-        file != "index.md" && splitext(file)[2] == ".md"
+# some paths for links
+readme_path = joinpath(project_path, "README.md")
+index_path = joinpath(project_path, "docs/src/index.md")
+license_url = "https://github.com/QEDjl-project/LaserExperiments.jl/blob/main/LICENSE"
+code_of_conduct_url = "https://github.com/QEDjl-project/LaserExperiments.jl/blob/main/CODE_OF_CONDUCT.md"
+
+# Copy README.md from the project base folder and use it as the start page
+open(readme_path, "r") do readme_in
+    readme_string = read(readme_in, String)
+
+    # replace relative links in the README.md
+    readme_string = replace(readme_string, "[MIT](LICENSE)" => "[MIT]($(license_url))")
+    readme_string = replace(readme_string, "(CODE_OF_CONDUCT.md)" => "($(code_of_conduct_url))")
+    readme_string = replace(readme_string, "[contributing guide directly on GitHub](docs/src/90-contributing.md)" => "contributing guide directly on GitHub")
+
+    open(index_path, "w") do readme_out
+        write(readme_out, readme_string)
+    end
+end
+
+pages = [
+    "Home" => "index.md",
+    "Contributing" => "90-contributing.md",
+    "Dev Docs" => "91-developer.md",
+    "References" => "95-reference.md",
 ]
 
-makedocs(;
-    modules = [LaserExperiments],
-    authors = "Uwe Hernandez Acosta <u.hernandez@hzdr.de>",
-    repo = Documenter.Remotes.GitHub("QEDjl-project", "LaserExperiments.jl"),
-    sitename = "LaserExperiments.jl",
-    #format = Documenter.HTML(; canonical = "https://QEDjl-project.github.io/LaserExperiments.jl"),
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://qedjl-project.gitlab.io/LaserExperiments.jl",
-        assets = String[],
-        mathengine = Documenter.MathJax2(),
-        collapselevel = 1,
-        edit_link = :commit,
-        # TODO: workaround
-        # should be fixed: https://github.com/QEDjl-project/QEDbase.jl/issues/4
-        size_threshold_ignore = ["index.md"],
-    ),
-    pages = ["index.md"; numbered_pages],
-)
+try
+    # generate docs with Documenter.jl
+    makedocs(;
+        modules = [LaserExperiments],
+        checkdocs = :exports,
+        authors = "Uwe Hernandez Acosta",
+        repo = Documenter.Remotes.GitHub("QEDjl-project", "LaserExperiments.jl"),
+        sitename = "LaserExperiments.jl",
+        format = Documenter.HTML(;
+            prettyurls = get(ENV, "CI", "false") == "true",
+            canonical = "https://qedjl-project.gitlab.io/LaserExperiments.jl",
+            assets = String[],
+            mathengine = Documenter.MathJax2(),
+            collapselevel = 1,
+        ),
+        pages = pages,
+    )
+finally
+    # doing some garbage collection
+    @info "GarbageCollection: remove generated landing page"
+    rm(index_path)
+end
 
-deploydocs(;
-    devbranch = "main",
-    repo = "github.com/QEDjl-project/LaserExperiments.jl",
-    push_preview = true
-)
+deploydocs(; repo = "github.com/QEDjl-project/LaserExperiments.jl.git", push_preview = false)
